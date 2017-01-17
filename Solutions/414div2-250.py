32c51d50c300a31ded279edc896026dfa6f6b85f
Iva Dobreva <iva95dobreva@gmail.com>
2017-01-18 05:30:00 +0900

    lazy

---
+++ Solutions/414div2-250.py
@@ -0,0 +1,15 @@
+class RestaurantManager:
+	def allocateTables(self, tables, groupSizes, arrivals, departures):
+		turned = 0
+
+
+
+		return turned
+
+t = (4)
+gs = (4,8,4,2,2,4)
+arr  = (0,10,12,16,18,26)
+dep = (10,20,18,26,36,28)
+
+x = RestaurantManager()
+print(x.allocateTables(t, gs, arr, dep))
