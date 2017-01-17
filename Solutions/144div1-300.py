fb19c62d492c0cd2033256c8493c95248737bc2e
Iva Dobreva <iva95dobreva@gmail.com>
2017-01-17 21:25:30 +0900

    Lazy commit

--- Solutions/144div1-300.py
+++ Solutions/144div1-300.py
@@ -1,35 +1,36 @@
+"""Bad example of solution """
 def test(val):
     if val<0 or val>1:
         return False
     return True
 
 class BinaryCode:
-  
+
     def decode(self, message):
-        
-        q = [int(e) for e in message]  
+
+        q = [int(e) for e in message]
         binary = ()
         q.pop()
 
         for first in (0, 1):
-            
+
             p = []
             str_result = ""
             idx = 0
             frst = True
 
             p.append(first)
-            
+
             for el in q:
                 if frst:
                     p.append(el-p[idx])
-                    
+
                     frst = False
                     idx += 1
                     if not test(p[idx]):
                          p = "NONE"
-                         break 
-                    
+                         break
+
                     continue
                 p.append(el - p[idx] - p[idx-1])
                 idx += 1
@@ -38,5 +39,5 @@ class BinaryCode:
                     break
 
             str_result = "".join(str(r) for r in p)
-            binary += (str_result, )  
+            binary += (str_result, )
         return tuple(binary)
